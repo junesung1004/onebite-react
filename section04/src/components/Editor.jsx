eb17c34ea1eb26b3f5361f0df2899656e988d38f
junesung1004 <junesung7979@gmail.com>
2024-03-11 00:34:36 +0900

    todo list 추가하기, 검색기능, 선택 후 삭제기능 구현 완료

--- section04/src/components/Editor.jsx
+++ section04/src/components/Editor.jsx
@@ -1,11 +1,40 @@
 import React from "react";
-import "../styles/Editor.css"
+import "../styles/Editor.css";
+import { useState, useRef } from "react";
 
-export default function Editor() {
+export default function Editor({ onCreate }) {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+
   return (
     <div className="Editor">
-      <input placeholder="새로운 Todo..." />
-      <button>추가</button>
+      <input
+        ref={contentRef}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+        value={content}
+        onKeyDown={onKeyDown}
+      />
+      <button onClick={onSubmit}>추가</button>
     </div>
   );
 }
